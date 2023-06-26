70ba2a0fce0fe1877218caf9a6ee11f45b1c402f
Avivcogo <avivcogo@gmail.com>
2023-06-26 15:47:13 +0200

    step2

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Person extends Model
 {
     use HasFactory;
+
+
+
+    public function Aliases(){
+        return $this->belongsToMany(Person::class);
+    
 }
+public function Images(){
+    return $this->belongsTo(Person::class);
+}
+
+public function statuses()
+{
+    return $this->belongsTo(Status::class);
+}
+
+}
+
